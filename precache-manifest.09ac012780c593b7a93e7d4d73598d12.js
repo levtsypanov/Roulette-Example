5de4cb5afa2dccdcf88e2a24fe754869d47c5ce1
levtsypanov <levdark7@gmail.com>
2020-02-20 21:57:22 +0300

    Updates

---
+++ precache-manifest.09ac012780c593b7a93e7d4d73598d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52edf777f7694eb2ea6f4b50e6a70cdd",
+    "url": "/Roulette-Example/index.html"
+  },
+  {
+    "revision": "531d001d73134ffc6427",
+    "url": "/Roulette-Example/static/css/2.87155a04.chunk.css"
+  },
+  {
+    "revision": "f32499da89ae6822a071",
+    "url": "/Roulette-Example/static/css/main.a447948e.chunk.css"
+  },
+  {
+    "revision": "531d001d73134ffc6427",
+    "url": "/Roulette-Example/static/js/2.e6c2cdbd.chunk.js"
+  },
+  {
+    "revision": "f32499da89ae6822a071",
+    "url": "/Roulette-Example/static/js/main.4580f41f.chunk.js"
+  },
+  {
+    "revision": "21f9bd491a1bb7c84416",
+    "url": "/Roulette-Example/static/js/runtime-main.412ce668.js"
+  }
+]);
